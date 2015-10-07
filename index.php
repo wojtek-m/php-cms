dc6d8dc54f35b7d16b912ab7844c299c427ef69e
Wojtek Murawski <wojciech.murawski@gmail.com>
2015-10-07 18:19:22 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li>About Us</li>
+            <li>History</li>
+            <li>Services</li>
+            <li>Gallery</li>
+        </ul>        
+    </nav>
+    
+    <main>
+        
+    </main>
+    
+    <aside>
+        
+    </aside>
+</body>
+</html>
